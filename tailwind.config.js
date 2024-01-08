ef5179bb6b434eeb256d2d5d193e8df055137ea3
MutebiAlvin <mutebialvinalvienzo@gmail.com>
2024-01-08 14:58:57 +0300

    Finsih Project

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./build/*.html","./build/js/*js"],
+  content: ["./build/*.html", "./build/js/*.js"],
   theme: {
     extend: {
+      colors: {
+        papayawhip: {
+          light: "#fef4e4",
+          DEFAULT: "#ffefd5",
+          dark: "#fee5bc",
+        },
+      },
       screens: {
         widescreen: { raw: "(min-aspect-ratio: 3/2)" },
-        tallscreen: { raw: "(min-aspect-ratio: 13/20)" },
+        tallscreen: { raw: "(max-aspect-ratio: 13/20)" },
       },
       keyframes: {
         "open-menu": {
           "0%": { transform: "scaleY(0)" },
-          "8%": { transform: "scaleY(1.2)" },
+          "80%": { transform: "scaleY(1.2)" },
           "100%": { transform: "scaleY(1)" },
         },
-        animation: {
-          "open-menu": "open-menu 0.5s ease-in-out forwards",
-        },
+      },
+      animation: {
+        "open-menu": "open-menu 0.5s ease-in-out forwards",
       },
     },
   },
